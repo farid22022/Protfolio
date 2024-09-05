b750203ee55838b8d15b9e3c420b7591df00839e
Farid <mdfaridhossenrehad@gmail.com>
2024-09-05 09:53:16 +0600

    Navbar is created

---
+++ src/Layout/Main/Main.jsx
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router-dom";
+import Header from "../../components/Header/Header";
+
+
+const Main = () => {
+
+    
+
+    return (
+        <div className="mx-auto ">
+            <Header></Header>
+            <Outlet></Outlet>
+        </div>
+    );
+};
+
+export default Main;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,64 @@
+import { Link } from "react-router-dom";
+import linkdin from "./../../../public/SocialIcon/linkedin.png"
+import github from "./../../../public/SocialIcon/github.png"
+
+const Header = () => {
+
+    const navOptions = <>
+        <li><Link className="text-xl">Home</Link></li>
+        <li><Link className="text-xl">Development Stack</Link></li>
+        <li><Link className="text-xl">Projects</Link></li>
+        <li><Link className="text-xl">Contact Me</Link></li>
+        <li><Link className="text-xl">About Me</Link></li>
+    </>
+
+    const linkOptions = <div className="flex space-x-5">
+        <li><Link><img src={linkdin}/></Link></li>
+        <li><Link><img src={github}/></Link></li>
+    </div>
+
+    return (
+        <div className="navbar bg-blue-800 p-5">
+            <div className="navbar-start">
+                <div className="dropdown">
+                <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                    <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor">
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M4 6h16M4 12h8m-8 6h16" />
+                    </svg>
+                </div>
+                <ul
+                    tabIndex={0}
+                    className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                    {
+                        navOptions
+                    }
+                </ul>
+                </div>
+                <a className="btn btn-ghost text-xl">Md. Farid Hossen Rehad</a>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                {
+                    navOptions
+                }
+                </ul>
+            </div>
+            <div className="navbar-end">
+                {
+                    linkOptions
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Header;
